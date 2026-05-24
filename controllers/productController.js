042b986a2b4faa03c48e58eb9113e29895af1f3e
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-05-24 23:33:12 +0300

    first

--- controllers/productController.js
+++ controllers/productController.js
@@ -31,6 +31,7 @@ exports.getFeaturedProducts = async (req, res) => {
 };
 
 
+exports.getProductById = async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (!product) return res.status(404).json({ message: "Product not found" });
   res.json(product);
