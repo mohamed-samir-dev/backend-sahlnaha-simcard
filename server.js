ce32529c8b77d7c104f6db9905b0cea519d6d367
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-05-25 04:17:09 +0300

    first

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ const adminRoutes = require("./routes/adminRoutes");
 connectDB();
 
 const app = express();
-const allowedOrigins = (process.env.FRONTEND_URL || "http://localhost:3000")
-  .split(",").map((o) => o.trim().replace(/\/+$/, ""));
+const allowedOrigins = [
+  "http://localhost:3000",
+  ...(process.env.FRONTEND_URL || "")
+    .split(",")
+    .map((o) => o.trim().replace(/\/+$/, ""))
+    .filter(Boolean),
+];
 
 app.use(cors({
   origin: (origin, cb) => {
